eb02b897b9da2fae4be2fab3ce3d124b67986e8b
Konrad Jedrol <konrad.jedrol@mydit.ie>
2014-11-13 16:24:07 +0000

    modified the py

--- list-was-queue.py
+++ list-was-queue.py
@@ -9,8 +9,16 @@ from boto.sqs.message import Message
 from boto.sqs.connection import SQSConnection
 from boto.exception import SQSError
 
-conn = boto.sqs.connect_to_region("us-east-1", aws_access_key_id='AKIAJ2BJXBF74JPNZKCQ', aws_secret_access_key='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')
+conn = boto.sqs.connect_to_region("us-east-1", aws_access_key_id='AKIAIR7EH3TNSTDUCWKA', aws_secret_access_key='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')
+
+conn2 = boto.sqs.connect_to_region("eu-west-1", aws_access_key_id='AKIAIR7EH3TNSTDUCWKA', aws_secret_access_key='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')
+
+NewQueue = conn2.create_queue("WhoUploadedTheKey(Again)?")
 
 rs = conn.get_all_queues()
+rs2 = conn2.get_all_queues()
 for q in rs:
+	print q.id
+  
+for q in rs2:
 	print q.id
