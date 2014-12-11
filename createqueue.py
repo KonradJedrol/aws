d65bd6a031a9d84d0f2e7aec7aa361b9aa4d4d09
KonradJedrol <c12509627@mydit.ie>
2014-12-11 23:47:22 +0000

    Create createqueue.py

---
+++ createqueue.py
@@ -0,0 +1,16 @@
+# This script created a queue
+#
+# Author - Paul Doyle Aug 2013
+#
+#
+import boto.sqs
+import boto.sqs.queue
+from boto.sqs.message import Message
+from boto.sqs.connection import SQSConnection
+from boto.exception import SQSError
+
+conn = boto.sqs.connect_to_region("us-east-1", aws_access_key_id='AKIAIR7EH3TNSTDUCWKA', aws_secret_access_key='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')
+
+conn2 = boto.sqs.connect_to_region("eu-west-1", aws_access_key_id='AKIAIR7EH3TNSTDUCWKA', aws_secret_access_key='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')
+
+Queue = conn2.create_queue("Such_queue_much_AWS")
