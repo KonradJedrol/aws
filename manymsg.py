02edf7179027ba7965336a8d5241bc8b4191fbda
KonradJedrol <c12509627@mydit.ie>
2014-12-11 23:28:18 +0000

    Create manymsg.py

---
+++ manymsg.py
@@ -0,0 +1,20 @@
+# This script creates a message
+#
+# Author - Paul Doyle Aug 2013
+#
+#
+import boto.sqs
+import boto.sqs.queue
+from boto.sqs.message import Message
+from boto.sqs.connection import SQSConnection
+from boto.exception import SQSError
+
+conn = boto.sqs.connect_to_region("us-east-1", aws_access_key_id='AKIAIR7EH3TNSTDUCWKA', aws_secret_access_key='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')
+
+conn2 = boto.sqs.connect_to_region("eu-west-1", aws_access_key_id='AKIAIR7EH3TNSTDUCWKA', aws_secret_access_key='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')
+
+Queue = conn2.get_queue("Such_queue_much_AWS")
+
+i=0
+for i in range(1,100):
+	conn.send_message(Queue, "Wow x " + str(i))
